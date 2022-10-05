26c7aac9e5a81a932a8c638dbffd5398e8555af9
hovanloi0810 <hovanloi0810@gmail.com>
2022-10-05 10:50:24 +0700

    completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import BaiTapThucHanhLayout from './BaiTapLayoutComponent/BaiTapThucHanhLayout';
+import './BaiTapLayoutComponent/style.css'
+
+function App() {
+  return (
+    <BaiTapThucHanhLayout/>
+  );
+}
+
+export default App;
---
+++ src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Body from './Body'
+import Header from './Header'
+import Footer from './Footer'
+
+const BaiTapThucHanhLayout = () => {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default BaiTapThucHanhLayout
